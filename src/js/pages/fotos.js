7efe044abc5934c752df8ff29ab281a1927bf0ba
Mateus Rubinho <onebyte81@gmail.com>
2025-03-09 03:20:49 -0400

    adicionado modal p/ fotos e botões para close

--- src/js/pages/fotos.js
+++ src/js/pages/fotos.js
@@ -3,3 +3,14 @@ fetch("src/pages/fotos.html").then(
 ).then(data => {
     document.getElementById("fotos").innerHTML = data;
 })
+function closeFoto(){
+    document.getElementById("imagemExibida").style.display = 'none';
+    document.getElementById("divImagem").style.visibility = 'hidden';
+}
+window.closeFoto = closeFoto;
+function abrirFoto(url){
+    document.getElementById('imagemExibida').src = url;
+    document.getElementById("imagemExibida").style.display = 'flex';
+    document.getElementById("divImagem").style.visibility = "visible";
+}
+window.abrirFoto = abrirFoto;
